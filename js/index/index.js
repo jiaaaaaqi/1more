ba98d0dd05a278b4f8eac6eddeafa831ce11a657
jiaqi <2468260248@qq.com>
2019-01-08 15:48:16 +0800

    首页暂时完美

--- js/index/index.js
+++ js/index/index.js
@@ -25,38 +25,6 @@ $(function () {
 
 /**banner效果***/
 
-
-//获得分辨率   可视区域高
-function keshi_gao() {
-    var h1 = document.documentElement.clientHeight;
-    return h1;
-}
-//获得分辨率  屏幕分辨率高度
-function body_gao() {
-    var h
-    if (!!(window.attachEvent && !window.opera))  //IE
-    {
-        h = document.documentElement.clientHeight + 20;
-    } else { h = window.innerHeight; }
-    return h;
-}
-//获得分辨率   可视区域宽
-function keshi_kuan() {
-    var w1 = document.documentElement.clientWidth;
-    return w1;
-}
-//获得分辨率  body对象宽度
-function body_kuan() {
-    var w = document.body.clientWidth;
-    return w;
-}
-//获取移动设备宽
-function kuan_yiding() {
-    var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-    return width;
-}
-
-
 //PC端加载
 function ladong() {
 	$(".logo3").show();
@@ -67,22 +35,10 @@ function ladong() {
 		$("#body_list").append(arrayImgBanner[i]);
 	}
 	var s;
-	/*var si = new switchImage();
-	si.title_id = "title_list";
-	si.body_id = "body_list";
-	si.current_class = "barh";
-	si.normal_class = "";
-	si.init();*/
 }
 //PC端加载
 window.onload = function () {
 	ladong();
-	/*
-    if (browserRedirect() == "pc") {
-        ladong()
-    } else {
-        hengshuping()
-    }*/
 }
 //PC端窗口上下拉动
 window.onresize = function () {
@@ -97,70 +53,7 @@ window.onresize = function () {
         $(this).css("height", h1 * 1); $(this).css("overflow", "hidden");
 
     });
-    //    $("#banner > ul > li").each(function () {
-    //        $(this).css("height", h1 * 1);
-
-    //    });
-
 
 }
-//手机端加载
-function ladong1() {
-    $(".top1").css("height", keshi_gao()*0.6);
-    $(".top1").css("min-height", keshi_gao()*0.5);
-    var tupian = $("#banner > ul > li:eq(0) > a > img");
-    var kuan = tupian.width();
-    var gao = tupian.height();
-    var h1 = body_kuan() * gao / kuan
-    if (h1 >= keshi_gao()) {
-        h1 = keshi_gao();
-    }
- 
-    $("#banner > ul > li > a > img").each(function () {
-        $(this).css("height", h1);
-        $(this).css("width", body_kuan());
-    });
-    $("#banner > ul > li").each(function () {
-        $(this).css("height", h1);
-        $(this).css("width", body_kuan());
-    });
-    $(".top").each(function () {
-        $(this).css("height", h1);
-        $(this).css("width", body_kuan());
-    });
-}
 
-//监听手机查看是横屏还是竖屏 
-function hengshuping() {
-    if (window.orientation == 180 || window.orientation == 0) {
-        //alert("竖屏状态")
-        ladong1()
-    }
-    if (window.orientation == 90 || window.orientation == -90) {
-        //alert("横屏状态")
-        ladong()
-    }
-}
-window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", hengshuping, false);
-
-//判断用户的浏览设备是移动设备还是PC
-function browserRedirect() {
-    var sUserAgent = navigator.userAgent.toLowerCase();
-    var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
-    var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
-    var bIsMidp = sUserAgent.match(/midp/i) == "midp";
-    var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
-    var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
-    var bIsAndroid = sUserAgent.match(/android/i) == "android";
-    var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
-    var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
-    //document.writeln("您的浏览设备为：");
-    if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
-        //alert("phone");
-        return "phone";
-    } else {
-        // alert("pc");
-        return "pc";
-    }
-}
 /**banner效果***/
